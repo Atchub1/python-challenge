d4c453b8da2d92f5936f9b14a83714d68f352a07
Atchub1 <amytcleveland@gmail.com>
2019-06-06 10:48:48 -0500

    fixing minor code error

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-csvpath = os.path.join('Resources', 'budget_data')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
